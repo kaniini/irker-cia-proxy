a7d653caf7acbd60df5b916644d7efe4356d2eaa
William Pitcock <nenolod@dereferenced.org>
2012-09-27 13:10:41 +0000

    Enhance file listing.

--- irker-cia-proxy.py
+++ irker-cia-proxy.py
@@ -13,27 +13,57 @@ implied. In no event shall the authors be liable for any damages arising
 from the use of this software.
 """
 
-import json, socket
+import json, socket, posixpath, re
 from xml.dom import minidom
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 
 target_server = "localhost"
 target_port = 6659
-template = "%(project)s %(author)s %(branch)s * %(revision)s / %(files)s: %(log)s"
+template = "%(bold)s%(project)s:%(bold)s %(green)s%(author)s%(reset)s %(yellow)s%(branch)s%(reset)s * r%(bold)s%(revision)s%(bold)s /%(files)s%(bold)s:%(bold)s %(log)s"
 
 projmap = json.load(open("projmap.json"))
 
 class CIAMessage:
     "Abstract class which represents a CIA message."
-    _files = []
     def __init__(self, messagexml):
         self._dom = minidom.parseString(messagexml)
+    def _render_files(self):
+        prefix, endings = self._consolidate_files()
+        endstr = ' '.join(endings)
+        if len(endstr) > 60:
+            endstr = self._summarize_files(endings)
+        if prefix.startswith('/'):
+            prefix = prefix[1:]
+        if endstr:
+            return "%s (%s)" % (prefix, endstr)
+        return prefix
+    def _consolidate_files(self):
+        files = []
         filenode = self.dig('message', 'body', 'commit', 'files')
         if filenode is not None:
             for child in filenode.childNodes:
                 if child.nodeName == 'file':
-                    self._files.append(self._shallowtext(child))
+                    files.append(self._shallowtext(child))
+        # Optimization: if we only have one file, don't waste CPU on any of the other
+        # stuff we do to pretend to be CIA.
+        if len(files) == 1:
+            return files[0], []
+        prefix = re.sub("[^/]*$", "", posixpath.commonprefix(files))
+        endings = []
+        for file in files:
+            ending = file[len(prefix):].strip()
+            if ending == '':
+                ending = '.'
+            endings.append(ending)
+        return prefix, endings
+    def _summarize_files(self, files):
+        dirs = {}
+        for file in files:
+            dirs[posixpath.split(file)[0]] = True
+        if len(dirs) <= 1:
+            return "%d files" % len(files)
+        return "%d files in %d dirs" % (len(files), len(dirs))
     def _shallowtext_generator(self, node):
         for child in node.childNodes:
             if child.nodeType == child.TEXT_NODE:
@@ -63,7 +93,13 @@ class CIAMessage:
             return text.strip()
         return None
     def data(self):
-        paths = {}
+        paths = {
+            'bold': '\x02',
+            'green': '\x033',
+            'blue': '\x032',
+            'yellow': '\x037',
+            'reset': '\x0F' 
+        }
         paths['project'] = self.lookup('message', 'source', 'project')
         paths['branch'] = self.lookup('message', 'source', 'branch')
         paths['module'] = self.lookup('message', 'source', 'module')
@@ -71,7 +107,7 @@ class CIAMessage:
         paths['version'] = self.lookup('message', 'body', 'commit', 'version')
         paths['author'] = self.lookup('message', 'body', 'commit', 'author')
         paths['log'] = self.lookup('message', 'body', 'commit', 'log')
-        paths['files'] = ' '.join(self._files)
+        paths['files'] = self._render_files()
         paths['url'] = self.lookup('message', 'body', 'commit', 'url')
         return paths
     def project(self):
